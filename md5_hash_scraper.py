ae3376a143c683caadc33da3e3aec05f5a023d36
awebb1 <38658476+awebb1@users.noreply.github.com>
2023-01-04 14:28:24 -0500

    Update md5_hash_scraper.py
    Added an optional argument for visuals. Slows it down though.

--- md5_hash_scraper.py
+++ md5_hash_scraper.py
@@ -7,55 +7,57 @@ import datetime
 hashes = ''
 
 # create a dict for args
-arg_name = ['script', 'file']
+arg_name = ['script', 'file', 'option1', 'option2']
 args = dict(zip(arg_name, sys.argv))
 
-help_msg = '\nPlease provide a file after the script:\n\t\033[1;31;40meg: \033[1;37;40mpython md5_hash_scraper.py \033[1;32;40mpath/to/file.sql\n\t    \033[1;37;40mpython md5_hash_scraper.py \033[1;32;40mpath/to/file.txt'
-
+help_msg = '\nPlease provide a file after the script:\n\t\033[1;31;40meg: \033[1;37;40mpython md5_hash_scraper.py \033[1;32;40mpath/to/file.sql\n\t    \033[1;37;40mpython md5_hash_scraper.py \033[1;32;40mpath/to/file.txt\n\n\033[1;37;40moptions (after file):\n\t -c, --count:\t will display a pretty visual of the number rising'
 
 # display help message if no args provided
 if len(args) < 2:
 	print(help_msg)
 	exit()
 
-# try except in case file is not found
 try:
 	count = 0
 
 	if args['file'] == '-h' or args['file'] == 'help' or args['file'] == '--help':
 		print(help_msg)
 		exit()
-		
 	file_name = sys.argv[1]
 
 	input_file = open(file_name)
 	file_content = input_file.read()
 
 	print("\n\033[1;36;40m[+] \033[1;37;40mFile loaded, attempting scrape...\n")
-	
-	# looping through hashes
+
 	if re.findall(r"([a-fA-F\d]{32})", file_content):
 		for hash in re.findall(r"([a-fA-F\d]{32})", file_content):
 			hashes += hash + '\n'
 			count += 1
 
+
+			if '-c' in args.values() or '--count' in args.values():
+				print('\r\033[1;35;40m[+] \033[1;33;40m' + str(count), end='')
+
+	if '-c' in args.values() or '--count' in args.values():
+		print('\r\033[1;35;40m[+] \033[1;33;40m' + str(count) + '\n\n', end='')
+
 	print("\033[1;36;40m[+] \033[1;37;40mScrape Complete\n")
-	
-	# determining output
+
 	if len(hashes) > 0:
 		hashes = hashes[:len(hashes) - 1]
 
 		if count < 11:
 			print("\033[1;36;40m[+] \033[1;37;40mHashes Found and Saved:\n\n" + hashes)
 		else:
-			print("\033[1;36;40m[+] \033[1;37;40mFound and Saved \033[1;31;40m" + str(count) + " \033[1;37;40mHashes")
+			print("\033[1;32;40m[!] \033[1;37;40mFound and Saved \033[1;31;40m" + str(count) + " \033[1;37;40mHashes")
 
 		if len(hashes) > 0:
 			f = open("scarped_hashes_" + str(datetime.datetime.now()) + ".txt", "w")
 			f.write(hashes)
 			f.close()
 	else:
-		print("\033[1;36;40m[+] \033[1;37;40mNo Hashes Found\n")
+		print("\033[1;31;40m[X] \033[1;37;40mNo Hashes Found\n")
 	input_file.close()
 
 except IOError:
