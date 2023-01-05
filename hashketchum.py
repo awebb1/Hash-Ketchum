861dca9f8a6908e3b675ea9bf1328b508752287f
awebb1 <38658476+awebb1@users.noreply.github.com>
2023-01-05 14:00:39 -0500

    Stdin Applied Correctly and Better Error Handling
    stdin can now be used with appropriate error handling. Fixed a hang up issue when script is called without and args, and added a new function at start of program that will handle custom error handling for files that do not exist.

--- hashketchum.py
+++ hashketchum.py
@@ -4,6 +4,7 @@ import re
 import sys
 import datetime
 import argparse
+import os
 
 HELP_MSG_COLOR = '\033[1;37;40m'
 ERROR_COLOR = '\033[1;31;40m'
@@ -13,8 +14,15 @@ COUNTING_COLOR = '\033[1;33;40m'
 GREEN_COLOR = '\033[1;32;40m'
 DEFAULT_COLOR = '\033[1;0;40m'
 
+def check_file_exists(file_path: str):
+	if not os.path.exists(file_path):
+		print(f"\n{ERROR_COLOR}[-] {HELP_MSG_COLOR}File not found")
+		exit()
+	return open(file_path, 'r')
+
 parser = argparse.ArgumentParser(description='Scrape MD5 hashes from a readable file', epilog=f'{ERROR_COLOR}eg:{DEFAULT_COLOR} hashketchum.py {GREEN_COLOR}path/to/file.sql{DEFAULT_COLOR} -c')
-parser.add_argument('file', help='the file to scrape hashes from')
+
+parser.add_argument('file', type=check_file_exists, nargs='?', help='the file to scrape hashes from')
 parser.add_argument('-c', '--count', action='store_true', help='display a pretty visual of the number rising')
 parser.add_argument('-rD', action='store_true', help='remove duplicates from list')
 
@@ -22,18 +30,28 @@ args = parser.parse_args()
 
 def main ():
 	try:
-	    md5()
+		md5()
 	except FileNotFoundError:
 	    print(f"\n{ERROR_COLOR}[-] {HELP_MSG_COLOR}File not found")
+	except PermissionError:
+		print(f"\n{ERROR_COLOR}[-] {HELP_MSG_COLOR}File is not accessible")
+	except ValueError:
+		parser.print_help()
 	except:
-	    print(f"\n{ERROR_COLOR}[-] {HELP_MSG_COLOR}An unknown error occurred")
+		print(f"\n{ERROR_COLOR}[-] {HELP_MSG_COLOR}An unknown error occurred")
 
 def md5():
 	count = 0
 	file_name = args.file
 
-	with open(file_name) as input_file:
-		file_content = input_file.read()
+	if args.file is None:
+		if sys.stdin.isatty():
+			raise ValueError
+		else:
+			file_content = sys.stdin.read()
+	else:
+		with args.file as input_file:
+			file_content = input_file.read()
 
 	print(f"\n{SUCCESS_COLOR}[+] {HELP_MSG_COLOR}File loaded, attempting scrape...")
 
@@ -76,6 +94,9 @@ def removeDuplicates(hash_list):
 	print(f"\r{GREEN_COLOR}[!] {ERROR_COLOR}" + str(length - len(hash_list)) + f" {HELP_MSG_COLOR}Duplicates Removed\n", end='')
 
 	return hash_list
+
+
+
 
 if __name__ == '__main__':
 	main()
