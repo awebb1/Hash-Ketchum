afdcb67c7e513a24a8228ec50c51951faa3c14db
awebb1 <38658476+awebb1@users.noreply.github.com>
2023-01-05 15:02:42 -0500

    Added comments
    Self explanatory.

--- hashketchum.py
+++ hashketchum.py
@@ -6,6 +6,7 @@ import datetime
 import argparse
 import os
 
+# declaring constants for colors
 HELP_MSG_COLOR = '\033[1;37;40m'
 ERROR_COLOR = '\033[1;31;40m'
 SUCCESS_COLOR = '\033[1;36;40m'
@@ -14,20 +15,27 @@ COUNTING_COLOR = '\033[1;33;40m'
 GREEN_COLOR = '\033[1;32;40m'
 DEFAULT_COLOR = '\033[1;0;40m'
 
+# function to get around argparse error when I implemented stdin. will be more than happy for other solutions to this
+#--------------------------------------------------------------------------------------------------------------------
 def check_file_exists(file_path: str):
 	if not os.path.exists(file_path):
 		print(f"\n{ERROR_COLOR}[-] {HELP_MSG_COLOR}File not found")
 		exit()
 	return open(file_path, 'r')
 
+# initialize argparse
 parser = argparse.ArgumentParser(description='Scrape MD5 hashes from a readable file', epilog=f'{ERROR_COLOR}eg:{DEFAULT_COLOR} hashketchum.py {GREEN_COLOR}path/to/file.sql{DEFAULT_COLOR} -c')
 
+# assign argument possibilities
 parser.add_argument('file', type=check_file_exists, nargs='?', help='the file to scrape hashes from')
 parser.add_argument('-c', '--count', action='store_true', help='display a pretty visual of the number rising')
 parser.add_argument('-rD', action='store_true', help='remove duplicates from list')
 
+# get args used
 args = parser.parse_args()
 
+# main function with various error handling
+#------------------------------------------------------------------
 def main ():
 	try:
 		md5()
@@ -40,11 +48,13 @@ def main ():
 	except:
 		print(f"\n{ERROR_COLOR}[-] {HELP_MSG_COLOR}An unknown error occurred")
 
+# md5 scraping function
+#----------------------------------------------------------------
 def md5():
 	count = 0
 	file_name = args.file
 
-	if args.file is None:
+	if args.file is None: # if statement used to prevent the script from hanging when no file and no stdin is provided, will just show the help menu
 		if sys.stdin.isatty():
 			raise ValueError
 		else:
@@ -55,25 +65,30 @@ def md5():
 
 	print(f"\n{SUCCESS_COLOR}[+] {HELP_MSG_COLOR}File loaded, attempting scrape...")
 
+	# using regex to find all hashes
 	hash_list = re.findall(r"([a-f\d]{32})", file_content, re.I)
+
+	# if statement for if hashes are or are not found
 	if hash_list:
 		print(f"{SUCCESS_COLOR}[+] {HELP_MSG_COLOR}Hashes Found\n", end='')
 
+		# if arg rD was used, this will remove duplicates from the list. Good when only wanting unique hashes, bad when trying to gauge frequency of hashes (obviously)
 		if args.rD:
 			hash_list = removeDuplicates(hash_list)
         
 		print(f"{SUCCESS_COLOR}[+] {HELP_MSG_COLOR}Processing File...")
 
-		for hash in hash_list:
-			count += 1
-			if args.count:
+		# literally just for the -c argument now. loop was utilized to build list before now it's solely cosmetic
+		if arg.count:
+			for hash in hash_list:
+				count += 1
 				print(f'\r{PROCESSING_COLOR}[+] {COUNTING_COLOR}{count}', end='')
 
-		if args.count:
 			print(f'\r{PROCESSING_COLOR}[+] {ERROR_COLOR}{count}\n', end='')
 
 	print(f"{SUCCESS_COLOR}[+] {HELP_MSG_COLOR}Complete\n")
 
+	# this if is just to check again after saying complete
 	if hash_list:
 		hashes = '\n'.join(hash_list)
 		with open("scraped_hashes_" + str(datetime.datetime.now()) + ".txt", "w") as f:
@@ -86,6 +101,8 @@ def md5():
 	else:
 		print(f"{ERROR_COLOR}[X] {HELP_MSG_COLOR}No Hashes Found")
 
+# this is the function called when argument rD is used. Made dedicated function for reusability later on
+#-------------------------------------------------------------------------------------------------------
 def removeDuplicates(hash_list):
 	length = len(hash_list)
 	print(f"\r{SUCCESS_COLOR}[+] {HELP_MSG_COLOR}Removing Any Duplicates...", end='')
@@ -94,9 +111,6 @@ def removeDuplicates(hash_list):
 	print(f"\r{GREEN_COLOR}[!] {ERROR_COLOR}" + str(length - len(hash_list)) + f" {HELP_MSG_COLOR}Duplicates Removed\n", end='')
 
 	return hash_list
-
-
-
-
+# call the main function
 if __name__ == '__main__':
 	main()
